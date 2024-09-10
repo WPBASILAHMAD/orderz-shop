711aa7b6b2584bf5348882948f5efe7ba3ea81e8
WPBASILAHMAD <basilmedia7@gmail,com>
2024-09-10 15:11:45 -0700

    Change the Local Host Link to Live Server

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -42,7 +42,7 @@ router.post(
 
       const activationToken = createActivationToken(seller);
 
-      const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+      const activationUrl = `https://orderz-shop-f.vercel.app/seller/activation/${activationToken}`;
 
       try {
         await sendMail({
